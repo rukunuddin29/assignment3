360952f4a355bb5f066ea032398a749fde094a37
rukunuddin29 <rukunuddin2003@gmail.com>
2025-04-17 16:00:59 +0530

    first commit

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function toggleMenu() {
   const nav = document.querySelector('.nav');
   nav.classList.toggle('active'); // Toggle the display of the nav links
 }
+
+ScrollReveal().reveal('.reveal', {
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  origin: 'bottom',
+  reset: false,
+  interval: 200
+});
