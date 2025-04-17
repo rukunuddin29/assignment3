ae3efb285d036a46e284f27b562753cd922e9b8d
rukunuddin29 <rukunuddin2003@gmail.com>
2025-04-17 15:16:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const typed = new Typed("#typed-text", {
+    strings: ["Connecting talented freelancers with opportunities that matter."],
+    typeSpeed: 40,
+    backSpeed: 0,
+    loop: false,
+    showCursor: false,
+  });
+  
